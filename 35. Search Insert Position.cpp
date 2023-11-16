ea2ba16a206c56d60fd82696a9b8d0e9f72a430d
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-11-16 14:46:14 +0200

    Create 35. Search Insert Position.cpp
    Pre-processing with binary search

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,24 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int len = nums.size();
+        if (target > nums[len - 1]) {
+            return len;
+        }
+        int left = 0, right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+};

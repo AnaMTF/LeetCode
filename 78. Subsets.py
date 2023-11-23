b520416b64073ba45addc0b150d0701aeeff9373
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-11-23 19:25:45 +0200

    Create 78. Subsets.py

---
+++ 78. Subsets.py
@@ -0,0 +1,25 @@
+# Given an integer array nums of unique elements, return all possible 
+# subsets
+#  (the power set).
+
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:  
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+        
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
